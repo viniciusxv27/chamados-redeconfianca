de646b73d33be72cd0ff2bcc210e747974b9484b
Vinicius <83793571+viniciusxv27@users.noreply.github.com>
2025-12-05 13:52:33 -0300

    daf

--- checklists/views.py
+++ checklists/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.http import JsonResponse
+from django.views.decorators.http import require_POST
 from django.db.models import Q, Count
 from django.core.paginator import Paginator
 from django.utils import timezone
