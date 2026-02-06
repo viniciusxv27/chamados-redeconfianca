4ceaf3ae41611a1a251da5fe8cf96bfff904d5d3
Vinicius <83793571+viniciusxv27@users.noreply.github.com>
2026-02-06 13:02:32 -0300

    add: up commission

--- users/commission_views.py
+++ users/commission_views.py
@@ -2355,14 +2355,6 @@ def commission_view(request):
     """
     user = request.user
     
-    # Bloqueio de acesso: apenas setores 24 e 29
-    SETORES_PERMITIDOS = [24, 29]
-    user_sector_id = user.sector.id if hasattr(user, 'sector') and user.sector else None
-    
-    if user_sector_id not in SETORES_PERMITIDOS:
-        from django.http import HttpResponseForbidden
-        return HttpResponseForbidden("Acesso negado. Você não tem permissão para acessar esta página.")
-    
     role = get_user_role(user)
     
     # Coordenador tem visão especial
